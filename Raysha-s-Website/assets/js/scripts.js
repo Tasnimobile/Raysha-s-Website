802c695ad321b908601de81fe7fbd1e214cfb4f6
Tasnimobile <tahsinulm2026@gmail.com>
2025-04-28 01:58:02 -0400

    Fixed Modal

--- Raysha-s-Website/assets/js/scripts.js
+++ Raysha-s-Website/assets/js/scripts.js
@@ -17,11 +17,13 @@ galleryItems.forEach(item => {
 // Close modal when the close button is clicked
 closeModal.addEventListener('click', () => {
     modal.style.display = 'none'; // Hide the modal
+    modalImage.src = ''; // Clear the modal image to prevent it from showing blank
 });
 
 // Close modal when clicking outside the image
 modal.addEventListener('click', (e) => {
     if (e.target === modal) {
         modal.style.display = 'none'; // Hide the modal
+        modalImage.src = ''; // Clear the modal image
     }
 });
